d99dd19f688fa40b3a24fee8e901a05a9641b607
Keven Kurz <tentoe86@gmail.com>
2020-05-31 07:31:35 +0000

    destignate, swift and storage server

--- README.md
+++ README.md
@@ -8,9 +8,12 @@
 
 ### designate
 
-<https://bugs.launchpad.net/kolla-ansible/+bug/1855877>
+- <https://bugs.launchpad.net/kolla-ansible/+bug/1855877>
+- <https://docs.openstack.org/kolla-ansible/train/reference/networking/designate-guide.html>
 
 - in designate-worker container remove id from /etc/designate/pools.yaml
-- `designate-manage pool upgrade --dry_run DRY_RUN` to get pool id
+- `designate-manage pool update`
+- `designate-manage pool update --dry_run DRY_RUN` to get pool id
 - replace designate_pool_id in passwords.yaml
 - `reconfigure -t designate`
+- `openstack zone create --email tentoe86@gmail.com local.kurz.site.`
---
+++ swift/setup.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+set -x #echo on
+
+STORAGE_NODES=(192.168.178.29)
+KOLLA_SWIFT_BASE_IMAGE="kolla/ubuntu-source-swift-base:train"
+mkdir -p /etc/kolla/config/swift
+
+CREATE_CONFIG="10 2 1"
+DISK_NUMS=(0 1 2 3)
+
+
+# Object Ring
+docker run \
+  --rm \
+  -v /etc/kolla/config/swift/:/etc/kolla/config/swift/ \
+  $KOLLA_SWIFT_BASE_IMAGE \
+  swift-ring-builder \
+    /etc/kolla/config/swift/object.builder create $CREATE_CONFIG
+
+for node in ${STORAGE_NODES[@]}; do
+    for i in ${DISK_NUMS[@]}; do
+      docker run \
+        --rm \
+        -v /etc/kolla/config/swift/:/etc/kolla/config/swift/ \
+        $KOLLA_SWIFT_BASE_IMAGE \
+        swift-ring-builder \
+          /etc/kolla/config/swift/object.builder add r1z1-${node}:6000/d${i} 1;
+    done
+done
+
+# Account Ring
+docker run \
+  --rm \
+  -v /etc/kolla/config/swift/:/etc/kolla/config/swift/ \
+  $KOLLA_SWIFT_BASE_IMAGE \
+  swift-ring-builder \
+    /etc/kolla/config/swift/account.builder create $CREATE_CONFIG
+
+for node in ${STORAGE_NODES[@]}; do
+    for i in ${DISK_NUMS[@]}; do
+      docker run \
+        --rm \
+        -v /etc/kolla/config/swift/:/etc/kolla/config/swift/ \
+        $KOLLA_SWIFT_BASE_IMAGE \
+        swift-ring-builder \
+          /etc/kolla/config/swift/account.builder add r1z1-${node}:6001/d${i} 1;
+    done
+done
+
+# Container Ring
+
+docker run \
+  --rm \
+  -v /etc/kolla/config/swift/:/etc/kolla/config/swift/ \
+  $KOLLA_SWIFT_BASE_IMAGE \
+  swift-ring-builder \
+    /etc/kolla/config/swift/container.builder create $CREATE_CONFIG
+
+for node in ${STORAGE_NODES[@]}; do
+    for i in ${DISK_NUMS[@]}; do
+      docker run \
+        --rm \
+        -v /etc/kolla/config/swift/:/etc/kolla/config/swift/ \
+        $KOLLA_SWIFT_BASE_IMAGE \
+        swift-ring-builder \
+          /etc/kolla/config/swift/container.builder add r1z1-${node}:6002/d${i} 1;
+    done
+done
+
+# Rebalance
+
+for ring in object account container; do
+  docker run \
+    --rm \
+    -v /etc/kolla/config/swift/:/etc/kolla/config/swift/ \
+    $KOLLA_SWIFT_BASE_IMAGE \
+    swift-ring-builder \
+      /etc/kolla/config/swift/${ring}.builder rebalance;
+done
+
+
